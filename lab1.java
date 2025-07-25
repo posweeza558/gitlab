45fd3f33c45709669667a3a143a1f2faf1c179e4
posweeza558 <possawee@kkumail.com>
2025-07-25 08:57:59 +0700

    skibidi2

--- lab1.java
+++ lab1.java
@@ -4,52 +4,47 @@ class Main {
   public static void main(String[] args) {
 
     char operator;
-    Double number1, number2, result;
+    Double numberone, numbertwo, result;
 
-    // create an object of Scanner class
-    Scanner input = new Scanner(System.in);
 
-    // ask users to enter operator
     System.out.println("Choose an operator: +, -, *, or /");
     operator = input.next().charAt(0);
 
-    // ask users to enter numbers
-    System.out.println("Enter first number");
-    number1 = input.nextDouble();
+    System.out.println("Enter number");
+    numberone = input.nextDouble();
 
     System.out.println("Enter second number");
-    number2 = input.nextDouble();
+    numbertwo = input.nextDouble();
 
     switch (operator) {
 
-      // performs addition between numbers
       case '+':
-        result = number1 + number2;
-        System.out.println(number1 + " + " + number2 + " = " + result);
+        result = numberone + numbertwo;
+        System.out.println(numberone + " + " + numbertwo + " = " + result);
         break;
 
-      // performs subtraction between numbers
       case '-':
-        result = number1 - number2;
+        result = numberone - numbertwo;
         System.out.println(number1 + " - " + number2 + " = " + result);
         break;
 
-      // performs multiplication between numbers
       case '*':
-        result = number1 * number2;
-        System.out.println(number1 + " * " + number2 + " = " + result);
+        result = numberone * numbertwo;
+        System.out.println(numberone + " * " + numbertwo + " = " + result);
         break;
 
-      // performs division between numbers
       case '/':
-        result = number1 / number2;
-        System.out.println(number1 + " / " + number2 + " = " + result);
+        result = numberone / numbertwo;
+        System.out.println(numberone + " / " + numbertwo + " = " + result);
         break;
 
       default:
-        System.out.println("Invalid operator!");
+        System.out.println("Invalid operator!!!");
         break;
     }
+    System.out.println("Thank for testing");
+    //Thank you
+    
 
     input.close();
   }
